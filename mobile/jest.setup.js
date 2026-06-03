0d09f4ab4f8e1f740e8201f63ae671353bc5562d
andy <nguyenvando510@gmail.com>
2026-06-03 23:46:16 +0700

    test(mobile): mock react-native-safe-area-context for header tests

--- mobile/jest.setup.js
+++ mobile/jest.setup.js
@@ -9,3 +9,14 @@ jest.mock('react-native-reanimated', () => ({
 jest.mock('expo-localization', () => ({
   getLocales: () => [{ languageCode: 'vi', languageTag: 'vi', regionCode: 'VN' }],
 }));
+
+jest.mock('react-native-safe-area-context', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+  return {
+    SafeAreaProvider: ({ children }) => React.createElement(View, null, children),
+    SafeAreaView:     ({ children }) => React.createElement(View, null, children),
+    useSafeAreaInsets: () => ({ top: 0, right: 0, bottom: 0, left: 0 }),
+    initialWindowMetrics: { insets: { top: 0, right: 0, bottom: 0, left: 0 }, frame: { x: 0, y: 0, width: 0, height: 0 } },
+  };
+});
