350ad9e4fcc0e816449ac641f7904acea62b51be
andy <nguyenvando510@gmail.com>
2026-06-06 11:10:16 +0700

    feat(mobile): add isPrivate to albumStore

--- mobile/src/stores/albumStore.ts
+++ mobile/src/stores/albumStore.ts
@@ -4,7 +4,13 @@ interface AlbumState {
   albumId: string | null;
   albumName: string | null;
   childBirthdate: string | null;
-  setAlbum: (album: { id: string; name: string; child_birthdate: string | null }) => void;
+  isPrivate: boolean | null;
+  setAlbum: (album: {
+    id: string;
+    name: string;
+    child_birthdate: string | null;
+    is_private: boolean;
+  }) => void;
   clearAlbum: () => void;
 }
 
@@ -12,7 +18,9 @@ export const useAlbumStore = create<AlbumState>((set) => ({
   albumId: null,
   albumName: null,
   childBirthdate: null,
-  setAlbum: ({ id, name, child_birthdate }) =>
-    set({ albumId: id, albumName: name, childBirthdate: child_birthdate }),
-  clearAlbum: () => set({ albumId: null, albumName: null, childBirthdate: null }),
+  isPrivate: null,
+  setAlbum: ({ id, name, child_birthdate, is_private }) =>
+    set({ albumId: id, albumName: name, childBirthdate: child_birthdate, isPrivate: is_private }),
+  clearAlbum: () =>
+    set({ albumId: null, albumName: null, childBirthdate: null, isPrivate: null }),
 }));
